eeac6c423ff863bf422001e86511bca162f7ea8a
HKGilchrist <hulseyckirk@gmail.com>
2023-11-02 10:03:18 -0600

    Adding featured puppy

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,37 @@ import './App.css'
 import { puppyList } from './data'
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log("puppyList: ", puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const [puppy, setPuppy] = useState(null)
 
+  function handleClick(puppyID) {
+    const featuredPup = puppies.find((pup)=> pup.id ===  puppyID)
+    setPuppy(featuredPup)
+    console.log(featuredPup)
+  }
 
   return (
-      <div className="App">
-        {puppies.map((puppy) => {
-          return <p>{puppy.name}</p>
-        })}
-
-      </div>
-  )
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={()=>{handleClick(puppy.id)}} key={puppy.id}>
+            {puppy.name}
+          </p>
+      
+        );
+      })}
+      { puppy && 
+         <div>
+         <h2>{puppy.name}</h2>
+         <ul>
+           <li>Age: {puppy.age}</li>
+           <li>Email: {puppy.email}</li>
+         </ul>
+       </div>
+      } 
+    </div>
+  );
 }
 
 export default App
