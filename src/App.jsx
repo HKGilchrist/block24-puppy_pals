6759f84fde9247468f975743e77d5cd4e3068371
HKGilchrist <hulseyckirk@gmail.com>
2023-11-02 09:16:39 -0600

    Linking js to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
+import { puppyList } from './data'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppyList: ", puppyList);
+
 
   return (
-    <>
-      <div>
-   
+      <div className="App">
+        {puppies.map((puppy) => {
+          return <p>{puppy.name}</p>
+        })}
+
       </div>
-    
-    </>
   )
 }
 
